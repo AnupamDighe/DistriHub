f096ac36e1cbaa493045fa340c8a8f0871cda3e8
Anupam Dighe <digheanupam@gmail.com>
2026-08-23 17:06:04 +0530

    updated

--- DistriHub/obj/Debug/net10.0/DistriHub.AssemblyInfo.cs
+++ DistriHub/obj/Debug/net10.0/DistriHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DistriHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93dc801fb9e26e00ac1576b2b13ed49a727ecf75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae068acf626c9531beea98d1cde5391ec288f94f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DistriHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DistriHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
